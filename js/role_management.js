162468d7d98400127f83e0146645a6100e15cc83
canvita <1904442782@qq.com>
2017-07-14 21:02:19 +0800

    role_management

--- js/role_management.js
+++ js/role_management.js
@@ -1,125 +1,275 @@
-//企业管理
- $(function() {
-       $("#enterprise").click(function() {
-            $('input[name="checkall"]').prop("checked",this.checked); 
-            $('input[name="subBox1"]').prop("checked",this.checked); 
-            $('input[name="subBox2"]').prop("checked",this.checked); 
-            $('input[name="subBox3"]').prop("checked",this.checked); 
-            $('input[name="subBox4"]').prop("checked",this.checked); 
-            $('input[name="subBox5"]').prop("checked",this.checked); 
-        });
-        var $checkall = $("input[name='checkall']");
-        var $subBox1 = $("input[name='subBox1']");
-        var $subBox2 = $("input[name='subBox2']");
-        var $subBox3 = $("input[name='subBox3']");
-        var $subBox4 = $("input[name='subBox4']");
-        var $subBox5 = $("input[name='subBox5']");
+/*
+  Function  :table test
+  Author    :zzx
+  Build_Date:2017-07-01
+  Version   :1.0
+  tips: 函数放置顺序为函数首字母字典序
+  执行顺序为firstLoad -> intoTable -> paging -> operate -> displayTotal
+  改变页面显示条数函数为displayOption,跳转到指定页函数为paging,查询函数为query,点击表头排序函数为sory
+*/
 
-        $checkall.click(function(){
-            $("#enterprise").prop("checked",$checkall.length == $("input[name='checkall']:checked").length ? true : false);
-        });
-        $subBox1.click(function(){
-            $("#enterprise").prop("checked",$subBox1.length == $("input[name='subBox1']:checked").length ? true : false);
-        });
-        $subBox2.click(function(){
-            $("#enterprise").prop("checked",$subBox2.length == $("input[name='subBox2']:checked").length ? true : false);
-        });
-        $subBox3.click(function(){
-            $("#enterprise").prop("checked",$subBox3.length == $("input[name='subBox3']:checked").length ? true : false);
-        });
-        $subBox4.click(function(){
-            $("#enterprise").prop("checked",$subBox4.length == $("input[name='subBox4']:checked").length ? true : false);
-        });
-        $subBox5.click(function(){
-            $("#enterprise").prop("checked",$subBox5.length == $("input[name='subBox5']:checked").length ? true : false);
-        });
+/*定义全部变量部分*/
 
-    });
+//定义传到php的数据对象
+var requestData={};
+//定义存放当前页面变量的对象
+var pageInfo = {};
 
-    $(function() {
-       $("#employee").click(function() {
-            $('input[name="subBox1"]').prop("checked",this.checked); 
-        });
-        var $subBox1 = $("input[name='subBox1']");
-        $subBox1.click(function(){
-            $("#employee").prop("checked",$subBox1.length == $("input[name='subBox1']:checked").length ? true : false);
-        });
-    } );
+/*函数部分*/
 
-     $(function() {
-       $("#role").click(function() {
-            $('input[name="subBox2"]').prop("checked",this.checked); 
-        });
-        var $subBox2 = $("input[name='subBox2']");
-        $subBox2.click(function(){
-            $("#role").prop("checked",$subBox2.length == $("input[name='subBox2']:checked").length ? true : false);
-        });
-    });
+/*选择每页显示条数函数*/
+var displayOption = function(){
+  //当select控件发生变化时触发
+  $('select').change(function(){
+    //获取选择的每页显示条数选项
+    requestData.disOption = $(this).val();
+    //当发生变化时跳转至第一页
+    requestData.curPage = 1;
+    intotable(requestData);
+  });   
+}
 
-   $(function() {
-	   $("#bed").click(function() {
-	        $('input[name="subBox3"]').prop("checked",this.checked); 
-	    });
-	    var $subBox3 = $("input[name='subBox3']");
-	    $subBox3.click(function(){
-	        $("#bed").prop("checked",$subBox3.length == $("input[name='subBox3']:checked").length ? true : false);
-	    });
-    });
+/*显示共有多少条记录函数*/
+var displayTotal = function(num){
+  if(num){
+  var txt1 = "共" + pageInfo.total + "条记录"
+  }
+  else{
+      txt1 = "0条记录"
+  }
+  $('#displayTotal').html(txt1);
+}
 
-    $(function() {
-	   $("#equipment").click(function() {
-	        $('input[name="subBox4"]').prop("checked",this.checked); 
-	    });
-	    var $subBox4 = $("input[name='subBox4']");
-	    $subBox4.click(function(){
-	        $("#equipment").prop("checked",$subBox4.length == $("input[name='subBox4']:checked").length ? true : false);
-	    });
-    });
+/*为初次载入时设置参数*/
+var firstLoad = function(){
+  //默认按照工号排序
+  requestData.sortType = 'role_name';
+  //默认每页显示10条
+  requestData.disOption = 10;
+  //默认为第一页开始
+  requestData.curPage = 1;
+  //默认搜索内容无并赋值0
+  requestData.query = 0;
+  //默认为升序排序
+  requestData.ascDesc = 0;
+  intotable(requestData);
+}
 
-    $(function() {
-	   $("#personal").click(function() {
-	        $('input[name="subBox5"]').prop("checked",this.checked); 
-	    });
-	   
-    });
-//会员服务
- $(function() {
-       $("#vip").click(function() {
-            $('input[name="checkall1"]').prop("checked",this.checked); 
-            $('input[name="subBox6"]').prop("checked",this.checked); 
-            $('input[name="subBox7"]').prop("checked",this.checked); 
-        });
-        var $checkall1 = $("input[name='checkall1']");
-        var $subBox6 = $("input[name='subBox6']");
-        var $subBox7 = $("input[name='subBox7']");
+//向表插入数据函数
+var intotable = function(requestData){ 
+  //从php获取json形式的数据,requestData为需要传到php的数据对象,得到的json数据存放在data中
+  $.post('../php/role_management.php',requestData,function(data){
+    if(data!=0){
+      //将json数据解析为js可读的数组形式
+      json =  JSON.parse(data);  
+      //初始化需要写入页面的html段落 
+      var html = '';
+      //遍历数组
+      $.each(json,function(index,value){ 
+        //写入html,并得到数据库的数据总数   
+        if(!value.total){
+             html += '<tr>';
+             html += '<td>' + value.role_name + '</td>';
+             html += '<td>' + value.role_description + '</td><td>';
+             html += '<a class="icon_a sys-color"><i class="fa fa-edit operateBtn"></i></a>';
+             html += '<a class="icon_a sys-color operateBtn"><i class="fa fa-info-circle operateBtn"></i></a>';
+             html += '<a class="icon_a warning-color operateBtn"><i class="fa fa-user-times operateBtn"></i></a>';
+             html += '</td>';
+             html += '</tr>';  
+          }      
+        else{
+          pageInfo.total = value.total;
+        }       
+      });
+      //插入html段落
+      $('tbody').html(html);
+      //显示总条数函数
+      displayTotal(1);
+      //执行分页函数
+      paging(1);
+      //执行操作函数
+      operate();
+      }
+      else{
+        //清空tbody
+        $('tbody').html("");
+        //设置页码对象的总条数为0
+        pageInfo.total = 0;
+        //不传参数执行下面两个函数
+        displayTotal();
+        paging();
+      }
+      });
+}
 
-        $checkall1.click(function(){
-            $("#vip").prop("checked",$checkall1.length == $("input[name='checkall1']:checked").length ? true : false);
-        });
-        $subBox6.click(function(){
-            $("#vip").prop("checked",$subBox6.length == $("input[name='subBox6']:checked").length ? true : false);
-        });
-        $subBox7.click(function(){
-            $("#vip").prop("checked",$subBox7.length == $("input[name='subBox7']:checked").length ? true : false);
-        });
+/*将从数据库得到的数字信息转化为可读信息,type为该数字要转化的信息类型,num为信息对应数字*/
+var numToString = function(type,num){
+  if(type == 'gender'){
+    if(num==1){
+      return '男';
+    }
+    else{
+      return '女';
+    }
+  }
+  else if(type == 'role_id'){
+    if(num ==1){
+      return '一般管理员';
+    }
+    else if(num == 2){
+      return '普通护工';
+    }
+    else if(num == 3){
+      return '高级护工';
+    }
+    else{
+      return '超级管理员';
+    }
+  }
+}
 
-    });
+/*操作函数*/
+var operate = function(){
+  //点击每行对应图标,获取该行唯一标识工号staff_serial_number
+  $('td i.operateBtn').click(function(e){
+    e.preventDefault();
+    var staff_id = $(this).parent().parent().attr('data-id');
+    window.location = 'staff_edit.html?staff_id=' + staff_id; 
+  });
+}
 
- $(function() {
-       $("#member").click(function() {
-            $('input[name="subBox6"]').attr("checked",this.checked); 
-        });
-        var $subBox6 = $("input[name='subBox6']");
-        $subBox6.click(function(){
-            $("#member").attr("checked",$subBox6.length == $("input[name='subBox6']:checked").length ? true : false);
-        });
-    });
- $(function() {
-       $("#nurse").click(function() {
-            $('input[name="subBox7"]').attr("checked",this.checked); 
-        });
-        var $subBox7 = $("input[name='subBox7']");
-        $subBox7.click(function(){
-            $("#nurse").attr("checked",$subBox7.length == $("input[name='subBox7']:checked").length ? true : false);
-        });
-    });
+/*分页设置函数 num用0和1表示有没有数据*/
+var paging = function(num){
+  //获取需要显示的总条数
+  var total = pageInfo.total;
+  //获取当前页面每页显示条数
+  var disOption = requestData.disOption;
+  //计算得到总页数
+  totalPage = Math.ceil(total/disOption); 
+  //初始化html
+  var html = '';
+  //插入前一页按钮
+  html += '<li id="previousPage">';        
+  html += '<a href="#" aria-label="Previous">';              
+  html += '<span aria-hidden="true">前一页</span>';             
+  html += '</a></li>'; 
+  //有数据显示页码
+  if(num){
+    if(totalPage<=5){
+      for(var i=1;i<=totalPage;i++){
+        html += '<li><a href="#">' + i + '</a></li>';
+      }
+    } 
+    //当总页数大于5时,部分显示页码           
+    else{
+      for(var i=1;i<=totalPage;i++){
+        if(i==1 || i==2){
+          html += '<li><a href="#">' + i + '</a></li>';
+        }
+        else if(i==3){
+          html += '<li><a>...</a></li>';
+        }
+        else if(i==totalPage){
+          html += '<li><a href="#">' + i + '</a></li>';
+        }
+      }
+    }
+  }
+  //当总页数小于6页时页码全部显示
+
+  //插入后一页按钮
+  html += '<li id="nextPage">'
+  html += '<a href="#" aria-label="Next">'  
+  html += '<span aria-hidden="true">后一页</span>';            
+  html += '</a></li>';
+  //插入分页html段落
+  $('ul.pagination').html(html).addClass('float-right margin-clear');
+  //获取当前页面
+  var curPage = requestData.curPage;
+  //禁用和启用前一页后一页
+  //当当前页为第一页时禁用前一页按钮
+  if(curPage == 1){
+    $('#previousPage').addClass('disabled');
+    //如果总页数为1,禁用后一页按钮
+    if(totalPage == 1 || pageInfo.total == 0){
+      $('#nextPage').addClass('disabled');
+    }
+  }
+  //如果当前页为最后一页,禁用后一页按钮
+  else if(curPage == totalPage){
+    $('#nextPage').addClass('disabled');
+  }
+  //不是首页和尾页,移除前一页和后一页的禁用
+  else{
+    $('a[aria-label]').removeClass('disabled');
+  }
+  //为按钮设置对应功能
+  $('ul.pagination a[href]').click(function(){
+    //获取页码并存入page变量
+    var page = $(this).text();
+    //当非首页时,点击前一页当前页码-1
+    if(page == '前一页' && curPage != 1){
+      requestData.curPage --;
+    }
+    //当为首页时,保持为首页
+    else if(page == '前一页' && curPage == 1){
+      requestData.curPage = 1;
+    }
+    //当非尾页时,点击后一页当前页码+1
+    else if(page == '后一页' && curPage != totalPage){
+      requestData.curPage ++;
+    }
+    //当为尾页时,保持为尾页
+    else if(page == '后一页' && curPage == totalPage){
+      requestData.curPage = totalPage;
+    }
+    //跳转至对应页码
+    else{
+      requestData.curPage = page; 
+    }   
+    intotable(requestData);
+  });
+}
+
+/*查询函数*/
+var query = function(){
+  //当输入内容时触发
+  $('#searchInput').keyup(function(){
+    //获取输入内容
+    requestData.query = $(this).val();
+    //当查询时跳转至第一页
+    requestData.curPage = 1;
+    intotable(requestData);   
+  })
+}
+
+/*排序函数*/
+var sort = function(){
+  //点击非操作列表头触发
+  $('thead th[data-type]').click(function(e){
+    //将获取到的排序关键字存入type变量
+    var type = $(this).attr('data-type');
+    //在页面插入上下箭头,升序时使用向上箭头表示,降序使用向下箭头表示
+    //去除原有的箭头
+    $(this).siblings().children().html('');
+    //如果已经为该列且为升序则改为降序
+    if(type == requestData.sortType && requestData.ascDesc < 1){
+      requestData.ascDesc += 1;
+      $(this).children().html('&#9660;')
+    }
+    //如果为降序或切换到该列排序时设置为升序,并且设置排序关键字为该列表头
+    else{
+      requestData.ascDesc = 0;
+      requestData.sortType = type;
+      $(this).children().html('&#9650;')
+    }   
+    //执行向表插入数据函数
+    intotable(requestData);
+  });
+}
+/*注册函数部分*/
+
+$(document).ready(displayOption);
+$(document).ready(firstLoad);
+$(document).ready(query);
+$(document).ready(sort);
--- php/role_management.php
+++ php/role_management.php
@@ -22,14 +22,14 @@ $query = $_REQUEST['query'];
 
 //编写sql语句
 $sql = "select * from"; 
-//如果没有查询内容,将数据库staff_info表的所有内容都获取  
+//如果没有查询内容,将数据库role_info表的所有内容都获取  
 if(!$query){
-	$sql .= " (select * from staff_info where staff_is_use=1 limit $pageStart,$disOption)";
+	$sql .= " (select * from role_info where role_is_use=1 limit $pageStart,$disOption)";
 }
 //当有查询内容时将查询到的内容返回
 else{
 	$query = '%'.strtolower($query).'%';
-	$sql .= " (select * from staff_info where lower(staff_serial_number) like '$query' or lower(staff_name) like '$query' or lower(staff_phone_num) like '$query' or lower(staff_id_card_no) like '$query' and staff_is_use=1 limit $pageStart,$disOption )";
+	$sql .= " (select * from role_info where lower(role_name) like '$query' and role_is_use=1 limit $pageStart,$disOption )";
 }
 $sql .= " t order by convert($sortType USING gbk)";
 if($ascDesc){
@@ -42,23 +42,23 @@ else{
 $rs = mysqli_query($conn,$sql);
 //将数据存入数组
 while($row = mysqli_fetch_array($rs)){
-$staff_infomation[] = array('staff_serial_number' => $row['staff_serial_number'],'staff_name' => $row['staff_name'],'staff_gender' => $row['staff_gender'],'staff_phone_num' =>$row['staff_phone_num'],'staff_id_card_no' => $row['staff_id_card_no'],'staff_role_id' => $row['staff_role_id'],'staff_create_time' => $row['staff_create_time'],'staff_id' => $row['staff_id']);
+$role_infomation[] = array('role_name' => $row['role_name'],'role_description' => $row['role_description']);
 }
-	//获取数据库staff_info表总记录条数
-	$sql = "select count(staff_serial_number) total from staff_info where staff_is_use=1";
+	//获取数据库role_info表总记录条数
+	$sql = "select count(role_id) total from role_info where role_is_use=1";
 	$row = fetchOne($sql);
 	//如果数据不为空,回传数据
-	if(!empty($staff_infomation)){
+	if(!empty($role_infomation)){
 		//如果正在查询,将总数设置为查询到的数据总条数
 		if($query){
-			$staff_infomation[] = array('total' => sizeof($staff_infomation));
+			$role_infomation[] = array('total' => sizeof($role_infomation));
 		}
 		//如果不在查询,将总数设置为表总记录条数
 		else{
-			$staff_infomation[] = array('total' => $row['total']);
+			$role_infomation[] = array('total' => $row['total']);
 		}
 		//将数组以json格式传回
-			echo json_encode($staff_infomation);
+			echo json_encode($role_infomation);
 	}
 	//如果数据为空回传0
 	else{
