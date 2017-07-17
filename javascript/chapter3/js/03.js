ef63ffbdc95125f1d17b1c650c74644e302dff7c
canvita <1904442782@qq.com>
2017-07-17 16:32:11 +0800

    123

--- javascript/chapter3/js/03.js
+++ javascript/chapter3/js/03.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-	txt = 'alert("hi")';
-	eval(txt);
+	function Person(name, age, job){
+		this.name = name;
+		this.age = age;
+		this.job = job;
+		this.friends = ["shelby","court"];
+		if(typeof this.sayName != "function"){
+			Person.prototype.sayName = function(){
+				alert(this.name);
+			};
+		}
+	}
+	var person1 = new Person("nico", 29, "Software Engineer");
+	var person2 = new Person("greg", 30, "Doctor");
+	person1.friends.push("van");
+	alert(person2.friends);
+	alert(person1.friends);
+	alert(person1.friends === person2.friends);
+	alert(person1.sayName === person2.sayName);
 });
 function searchInStr(string,target){
 	var position = [];
@@ -10,4 +26,7 @@ function searchInStr(string,target){
 		pos = string.indexOf(target,pos+1);
 	}
 	return position;
+}
+function hasPrototypeProperty(object,name){
+	return !object.hasOwnProperty(name) && (name in object);
 }
